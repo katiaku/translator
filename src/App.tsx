b62c8a48407c11f0b5ca0bc08ed195ba8d27a0ec
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-07-11 20:02:10 +0200

    Add initial logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { useStore } from './hooks/useStore';
 
 function App() {
+
+  const { fromLanguage, setFromLanguage } = useStore();
   return (
     <div className='App'>
       <h1>Translator</h1>
+      <button onClick={() => {
+        setFromLanguage("es")
+      }}>Change To Spanish</button>
+      {fromLanguage}
     </div>
   )
 }
---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const SUPPORTED_LANGUAGES = {
+    en: "English",
+    es: "Spanish",
+    jp: "Japanese"
+};
+
+export const AUTO_LANGUAGE = "auto";
